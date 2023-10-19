e9958fb59eca1c7ff5b7e72d314b5c9ba459f68e
Mike <mreickcastillero@gmail.com>
2023-10-18 22:58:55 -0700

    added major functionality to index.js, need to work on generateMarkdown next and begin compiling files into index

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const { title } = require("process");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(fileName, generateMarkdown(data), (err)=>
+    err ? console.error(err) : console.log('Sucess! Go checkout your new ReadMe!'))
+    // (generateMarkdown(data))
+}
+//use FS module to write data to readme file filename
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+.prompt([
+{
+type: "input",
+message: "What is the title of your project?",
+name: "title"
+},
+{
+type: "input",
+message: "Please enter any information you would like to include in your Description section:",
+name: "Description"
+},
+{
+type: "input",
+message: "Please enter any instructions you would like to include in your Installation section:",
+name: "Installation"
+},
+{
+type: "input",
+message: "Please enter any information you would like to include in your Usage section:",
+name: "Usage"
+},
+{
+type: "input",
+message: "Please explain your projects contribution guidlines:",
+name: "Contributing",
+},
+{
+type: "input",
+message: "Please enter any information regarding tests:",
+name: "Tests"
+},
+{
+type: "checkbox",
+message: "What license did you use for your project?",
+choices: ["MIT License", "Apache License 2.0", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0"],
+name: "license"
+},
+{
+type: "input",
+message: "Please enter your Github username:",
+name: "Username"
+},
+{
+type: "input",
+message: "Please enter your email address:",
+name: "Contact"
+},
+])
+.then((response) => {
+    console.log("response", response)
+    writeToFile("NEWREADME.md", response)
+})
+}
 
 // Function call to initialize app
 init();
+
+
+
+
+//use inquirer to display questions to user
+//call generatemarkdown function on the data collect
+//take what is returned to us, then pass it to the writeToFile function
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+const fs = require("fs")
 function renderLicenseBadge(license) {}
+//have a couple of license options
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  license = data.License
+  var licenseLink;
+  if(license = "MIT License"){
+    licenseLink = "https://mit-license.org/"
+  }
+  else if(license = "Apache License 2.0"){
+    licenseLink = "https://www.apache.org/licenses/LICENSE-2.0"
+  }
+  else if(license ="BSD 3-Clause 'New' or 'Revised' License"){
+    licenseLink = "https://choosealicense.com/licenses/bsd-3-clause/"
+  }
+}
+//return the link to the license
+//could use a switch or if statements
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
+//create conditional statement checking if license exists
+//check readme template to see what is needed here
+//call renderLicenseBadge and renderLicenseLink 
+//return badge, link, and extra sting content together as a template literal
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.Description}
+
+  ## Table of Contents
+
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contribution-Guidelines](#Contribution-Guidelines)
+  * [Tests](#Tests)
+  * [License](#License)
+  * [Questions](#Questions)
+
+  ## Installation
+
+  ${data.Installation}
+
+  ## Usage
+
+  ${data.Usage}
+
+  ## Contribution-Guidelines
+
+  ${data.Contributing}
+
+  ## Tests
+
+  ${data.Tests}
+
+  ## License
+
+  ${data.license}
+
+  ## Questions
+
+If you have anymore questions please reach out to me at my email: ${data.Contact}, or visit my [Github Account](https://github.com/${data.Username})
 `;
 }
+
+//call license functions in generate markdown
+//create layour of what you want as a readme layout with template literals
 
 module.exports = generateMarkdown;
